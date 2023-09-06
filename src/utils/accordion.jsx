8b49f9499a31c615da1d323aa97cd9f8d7186a40
AkashJana18 <akashjana663@gmail.com>
2023-09-06 13:31:04 +0530

    Initial commit

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Access the most competitive rates",
+    detail:
+      "Empower Your Professional Journey: Explore the Best Courses with Unbeatable Rates on Our Educational Platform, helping you maximize your skills and career potential.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Stabilize your career trajectory",
+    detail:
+      "Stabilize your career trajectory with our carefully curated courses, designed to equip you with the skills and knowledge needed for long-term success in your chosen field.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best-in-class courses on the market",
+    detail:
+      "Experience the pinnacle of educational excellence with our best-in-class courses, crafted to provide you with the highest quality learning opportunities available in the market.",
+  },
+];
+export default data;
